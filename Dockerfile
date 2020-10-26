d046421d56fd88f1d271635c4c943fda98f2a891
Yongqin Liu <yongqin.liu@linaro.org>
2020-10-26 19:17:59 +0800

    update to use one line for more commands and instructions on docker hub
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- Dockerfile
+++ Dockerfile
@@ -4,16 +4,8 @@ FROM ubuntu:18.04
 # Set the working directory.
 WORKDIR /linaro-android
 
-RUN apt-get update
-RUN apt-get install -y apt-utils unzip wget git
+RUN apt-get update && apt-get install -y apt-utils unzip wget git
 
 ADD https://dl.google.com/android/repository/platform-tools_r28.0.0-linux.zip .
 RUN unzip platform-tools_r28.0.0-linux.zip
-RUN rm -f /usr/bin/fastboot /usr/bin/adb
-RUN ln -s /linaro-android/platform-tools/fastboot /usr/bin/fastboot
-RUN ln -s /linaro-android/platform-tools/adb /usr/bin/adb
-RUN ls -l /usr/bin/adb /usr/bin/fastboot
-RUN fastboot --version
-RUN adb --version
-
-
+RUN rm -f /usr/bin/fastboot /usr/bin/adb && ln -s /linaro-android/platform-tools/fastboot /usr/bin/fastboot && ln -s /linaro-android/platform-tools/adb /usr/bin/adb
--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@
 
 #docker build --tag bulletinboard:1.0 .
 #docker run --publish 8000:8080 --detach --name bb bulletinboard:1.0
-docker build --tag linaro-android:0.1 . -f Dockerfile  # --tag is to define the image name
+#docker build --tag linaro-android:0.1 . -f Dockerfile  # --tag is to define the image name
+docker build --tag yongqinliu/linaro-android-docker:0.1 . -f Dockerfile  # --tag is to define the image name
 # docker run --name test-android linaro-android:0.1 # --name is used to specified the container name
-# docker run --interactive --tty linaro-android:0.1 bash # name will be generated automatically
+# docker run --interactive --tty --rm linaro-android:0.1 bash # name will be generated automatically
+# 		# --rm remove automatically when exit
 # docker image ls # list docker images
 # docker ps --all # list containers
 # docker login
